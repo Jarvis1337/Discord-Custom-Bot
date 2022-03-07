11435876fce21a19e09e4a9f06cc876eece465ca
Your's Jarvis <77638593+Yours-Jarvis@users.noreply.github.com>
2022-03-07 18:04:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send("🚀 Please follow on GitHub to stay tuned with us for more Exciting future Updates like this. | © 2021 — Made By Your's Jarvis #2431 with ♥"))
+app.get('/', (req, res) => res.send('<meta http-equiv="refresh" content="0; URL=https://yours-jarvis.github.io/Yours-Jarvis"/>))
 
 app.listen(port, () =>
 console.log(`Your app is listening a http://localhost/${port}`)
@@ -22,7 +22,7 @@ const config = require('./config.json')
 client.on("ready", () => {
   client.user.setStatus("dnd")
   function randomStatus() {
-    let status = ["Bot Host By Your's Jarvis - https://github.com/Yours-Jarvis"]
+    let status = ["Bot Host By Your's Jarvis - https://github.com/Yours-Jarvis", "https://yours-jarvis.github.io/Yours-Jarvis/"]
     let rstatus = Math.floor(Math.random() * status.length);
 
 
